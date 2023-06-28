c03f95865bfeeaa3335d5972b45eb9e6c4bf5a73
Holden Schermer <holden@holdens-mbp.lan>
2023-06-27 23:01:47 -1000

    fully working search function

--- src/monkseal/controller.js
+++ src/monkseal/controller.js
@@ -3,133 +3,117 @@ const queries = require("./queries");
 const { head } = require("./routes");
 
 const getMonkseals = (req, res) => {
-    pool.query(queries.getMonkseals, (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
+  pool.query(queries.getMonkseals, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
 };
 
 const getSealById = (req, res) => {
-    const id = parseInt(req.params.id);
-    pool.query(queries.getSealById, [id], (error, results) => {
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    });
+  const id = parseInt(req.params.id);
+  pool.query(queries.getSealById, [id], (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results.rows);
+  });
 };
 
 const addSeal = (req, res) => {
-    const { sealid, name, size, sex, headscar } = req.body;
-    // check if email exists
-    pool.query(queries.checkSealIdExists, [sealid], (error, results) => {
-        if (results.rows.length) {
-            res.send("Seal ID already exists.");
-        }
-        
-        // add seal to database
-        pool.query(queries.addSeal, [sealid, name, size, sex, headscar], (error, results) => {
-            if (error) throw error;
-            res.status(201).send("Seal profile added successfully!");
-        });
-    });
+  const { sealid, name, size, sex, headscar } = req.body;
+  // check if email exists
+  pool.query(queries.checkSealIdExists, [sealid], (error, results) => {
+    if (results.rows.length) {
+      res.send("Seal ID already exists.");
+    }
+
+    // add seal to database
+    pool.query(
+      queries.addSeal,
+      [sealid, name, size, sex, headscar],
+      (error, results) => {
+        if (error) throw error;
+        res.status(201).send("Seal profile added successfully!");
+      }
+    );
+  });
 };
 
 const removeSeal = (req, res) => {
-    const id = parseInt(req.params.id);
-    
-    pool.query(queries.getSealById, [id], (error, results) => {
-        const noSealFound = !results.rows.length;
-        if(noSealFound){
-            res.send("Seal does not exist in the database.");
-        }
+  const id = parseInt(req.params.id);
 
-        pool.query(queries.removeSeal, [id], (error, results) => {
-            if (error) throw error;
-                res.status(200).send("Seal removed successfully.");
-        });
+  pool.query(queries.getSealById, [id], (error, results) => {
+    const noSealFound = !results.rows.length;
+    if (noSealFound) {
+      res.send("Seal does not exist in the database.");
+    }
+
+    pool.query(queries.removeSeal, [id], (error, results) => {
+      if (error) throw error;
+      res.status(200).send("Seal removed successfully.");
     });
+  });
 };
 
 const updateSeal = (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name } = req.body;   
+  const id = parseInt(req.params.id);
+  const { name } = req.body;
 
-    pool.query(queries.getSealById, [id], (error, results) => {
-        const noSealFound = !results.rows.length;
-        if(noSealFound) {
-            res.send("Seal does not exist in the database.");
-        }
+  pool.query(queries.getSealById, [id], (error, results) => {
+    const noSealFound = !results.rows.length;
+    if (noSealFound) {
+      res.send("Seal does not exist in the database.");
+    }
 
-        pool.query(queries.updateSeal, [name, id], (error, results) => {
-            if (error) throw error;
-            res.status(200).send("Seal updated successfully!");
-        });
+    pool.query(queries.updateSeal, [name, id], (error, results) => {
+      if (error) throw error;
+      res.status(200).send("Seal updated successfully!");
     });
+  });
 };
 
-const findSeal = (req, res) => {
-    const { sealid, name, size, sex, headscar } = req.body;
-    
-    // Build an array to hold the search parameters
-    const searchParams = [];
-    let searchQuery = 'SELECT * FROM accounts WHERE';
-    
-    // Check if seal ID is provided
-    if (sealid) {
-      searchParams.push(sealid);
-      searchQuery += ' sealid = $' + searchParams.length + ' AND';
-    }
-    
-    // Check if name is provided
-    if (name) {
-      searchParams.push(name);
-      searchQuery += ' name = $' + searchParams.length + ' AND';
-    }
-    
-    // Check if size is provided
-    if (size) {
-      searchParams.push(size);
-      searchQuery += ' size = $' + searchParams.length + ' AND';
-    }
-    
-    // Check if sex is provided
-    if (sex) {
-      searchParams.push(sex);
-      searchQuery += ' sex = $' + searchParams.length + ' AND';
-    }
-    
-    // Check if headscar is provided
-    if (headscar) {
-      searchParams.push(headscar);
-      searchQuery += ' headscar = $' + searchParams.length + ' AND';
-    }
-    
-    // Remove the trailing 'AND' from the query
-    searchQuery = searchQuery.slice(0, -4);
-    
-    // Check if any search parameters were provided
-    if (searchParams.length === 0) {
-      res.send("No search parameters provided.");
-      return;
-    }
-    
-    // Find seals based on search parameters
-    pool.query(searchQuery, searchParams, (error, results) => {
-      if (error) {
-        console.error('Error finding seals:', error);
-        res.status(500).send("An error occurred while finding seals.");
-      } else {
-        res.status(200).send(results.rows);
-      }
-    });
-  };
 
+const findSeal = (req, res) => {
+  const searchParams = [];
+  const conditions = [];
+
+  const params = {
+    sealid, name, size, sex, birthyear, birthisland, tagcolor, lefttag, righttag,
+    identifiers, headfacescar, dorsalscar, ventralscar, rightlateralscar, leftlateralscar,
+    rightforeflipperscar, leftforeflipperscar, hindflippersscar, scardetail,
+    headfacebleach, dorsalbleach, ventralbleach, rightlateralbleach, leftlateralbleach,
+    rightforeflipperbleach, leftforeflipperbleach, hindflippersbleach, bleachdetail,
+    hawaiisighting, kauaisighting, molokaisighting, mauisighting, lanaisighting, oahusighting
+  } = req.body;
+
+  for (const [key, value] of Object.entries(params)) {
+    if (value) {
+      searchParams.push(value);
+      conditions.push(`${key} = $${searchParams.length}`);
+    }
+  }
+
+  if (searchParams.length === 0) {
+    res.send("No search parameters provided.");
+    return;
+  }
+
+  const searchQuery = `SELECT * FROM accounts WHERE ${conditions.join(" AND ")}`;
+
+  pool.query(searchQuery, searchParams, (error, results) => {
+    if (error) {
+      console.error("Error finding seals:", error);
+      res.status(500).send("An error occurred while finding seals.");
+    } else {
+      res.status(200).send(results.rows);
+    }
+  });
+};
 
 
 module.exports = {
-    getMonkseals,
-    getSealById,
-    addSeal,
-    removeSeal,
-    updateSeal,
-    findSeal,
+  getMonkseals,
+  getSealById,
+  addSeal,
+  removeSeal,
+  updateSeal,
+  findSeal,
 };

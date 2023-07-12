f2db45422b6a24a5d18313505c7825ea28021cce
keskami <kkak808@gmail.com>
2023-07-12 10:22:54 -1000

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const monksealRoutes = require('./src/monkseal/routes');""
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors({ origin: true }));
